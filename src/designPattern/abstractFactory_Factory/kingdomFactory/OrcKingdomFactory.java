793a6642e4aeb30d1cd8129a72c766fa5cbc639d
dravit <dravit.info@gmail.com>
2018-03-27 14:34:19 +0530

    Add files via upload

--- src/designPattern/abstractFactory_Factory/kingdomFactory/OrcKingdomFactory.java
+++ src/designPattern/abstractFactory_Factory/kingdomFactory/OrcKingdomFactory.java
@@ -1,26 +1,25 @@
 package designPattern.abstractFactory_Factory.kingdomFactory;
 
-import designPattern.abstractFactory_Factory.elevenKingdom.ElfArmy;
-import designPattern.abstractFactory_Factory.elevenKingdom.ElfCastle;
-import designPattern.abstractFactory_Factory.elevenKingdom.ElfKing;
 import designPattern.abstractFactory_Factory.interfaces.Army;
 import designPattern.abstractFactory_Factory.interfaces.Castle;
 import designPattern.abstractFactory_Factory.interfaces.King;
 import designPattern.abstractFactory_Factory.interfaces.KingdomFactory;
+import designPattern.abstractFactory_Factory.orcishKingdom.OrcArmy;
+import designPattern.abstractFactory_Factory.orcishKingdom.OrcCastle;
+import designPattern.abstractFactory_Factory.orcishKingdom.OrcKing;
 
 /**
  * Created by guptdbh on 27/03/2018.
  */
-public class ElfKingdomFactory implements KingdomFactory {
+public class OrcKingdomFactory implements KingdomFactory {
+
     public Castle createCastle() {
-        return new ElfCastle();
+        return new OrcCastle();
     }
-
     public King createKing() {
-        return new ElfKing();
+        return new OrcKing();
     }
-
     public Army createArmy() {
-        return new ElfArmy();
+        return new OrcArmy();
     }
 }
