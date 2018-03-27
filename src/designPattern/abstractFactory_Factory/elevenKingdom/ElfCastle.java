93f4a84a0f347574c4ab99ec0365f66432ec7bc7
dravit <dravit.info@gmail.com>
2018-03-27 14:32:02 +0530

    Add files via upload

--- src/designPattern/abstractFactory_Factory/elevenKingdom/ElfCastle.java
+++ src/designPattern/abstractFactory_Factory/elevenKingdom/ElfCastle.java
@@ -1,13 +1,13 @@
 package designPattern.abstractFactory_Factory.elevenKingdom;
 
-import designPattern.abstractFactory_Factory.interfaces.Army;
+import designPattern.abstractFactory_Factory.interfaces.Castle;
 
 /**
  * Created by guptdbh on 27/03/2018.
  */
-public class ElfArmy implements Army {
+public class ElfCastle implements Castle {
 
-    private static final String DESCRIPTION = "This is Elven Army !";
+    private static final String DESCRIPTION = "This is Elven Castle !";
     @Override
     public String getDescription() {
         return DESCRIPTION;
--- src/designPattern/abstractFactory_Factory/elevenKingdom/ElfKing.java
+++ src/designPattern/abstractFactory_Factory/elevenKingdom/ElfKing.java
@@ -1,13 +1,13 @@
 package designPattern.abstractFactory_Factory.elevenKingdom;
 
-import designPattern.abstractFactory_Factory.interfaces.Army;
+import designPattern.abstractFactory_Factory.interfaces.King;
 
 /**
  * Created by guptdbh on 27/03/2018.
  */
-public class ElfArmy implements Army {
+public class ElfKing implements King {
 
-    private static final String DESCRIPTION = "This is Elven Army !";
+    private static final String DESCRIPTION = "This is Elven King !";
     @Override
     public String getDescription() {
         return DESCRIPTION;
